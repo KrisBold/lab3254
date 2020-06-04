cd9485461ea2b6715f0f6225b827e540aa14d5e8
Kristina <kristyaboldyreva@yandex.ru>
2020-06-04 13:35:04 +0700

    нереализованный класс стратегия по типам

---
+++ filetypestrategy.cpp
@@ -0,0 +1,1 @@
+#include "filetypestrategy.h"
---
+++ filetypestrategy.h
@@ -0,0 +1,12 @@
+#ifndef FILETYPESTRATEGY_H
+#define FILETYPESTRATEGY_H
+#define FOLDERSTRATEGY_H
+#include "strategy.h"
+
+class FileTypeStrategy
+{
+public:
+
+};
+
+#endif // FILETYPESTRATEGY_H
